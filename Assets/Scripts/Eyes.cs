d598939cf02c6b39ef5d8cc28bbd2f0bd6c23e88
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-02-15 21:02:51 +0530

    Added Zoom button

--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Eyes : MonoBehaviour {
 
+	private Camera eyes;
+	private float defaultPOV;
 	// Use this for initialization
 	void Start () {
-		
+		eyes = GetComponent<Camera> ();
+		defaultPOV = eyes.fieldOfView;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetButton("Zoom")){
+			eyes.fieldOfView = defaultPOV / 1.5f;
+		}else{
+			eyes.fieldOfView = defaultPOV;
+		}
 		
 	}
 }
