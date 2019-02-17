7e083118f08f943d7d1adc7cc1a351ce486875c9
ArjunMahesh97 <arjunmahesh97@gmail.com>
2019-02-18 01:13:00 +0530

    Added Helicopter

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -7,16 +7,19 @@ public class Helicopter : MonoBehaviour {
 	public AudioClip callSound;
 
 	private bool called = false;
+	private AudioSource audiosource;
 
 	// Use this for initialization
 	void Start () {
-		
+		audiosource = GetComponent<AudioSource> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("CallHeli") && !called){
 			called=true;
+			audiosource.clip = callSound;
+			audiosource.Play ();
 		}
 	}
 }
